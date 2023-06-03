5744c5ab2025556026a30a343171cd2f3f992534
Raj Bhatia <97431540+BhatiaRaj@users.noreply.github.com>
2023-06-03 20:54:19 +0530

    Work on PopUp Model

--- CarPooling/Controllers/OfferingCarpoolingController.cs
+++ CarPooling/Controllers/OfferingCarpoolingController.cs
@@ -7,19 +7,19 @@ namespace CarPooling.Controllers
 {
     public class OfferingCarpoolingController : Controller
     {
-        //private readonly CarpoolingDbContext carpoolingDb;
+        private readonly CarpoolingDbContext carpoolingDb;
 
-        //public OfferingCarpoolingController(CarpoolingDbContext carpoolingDb)
-        //{
-        //    this.carpoolingDb = carpoolingDb;
-        //}
+        public OfferingCarpoolingController(CarpoolingDbContext carpoolingDb)
+        {
+            this.carpoolingDb = carpoolingDb;
+        }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            //var CarPooling = await carpoolingDb.OppernigCarpooling.ToListAsync();
-            //return View(CarPooling);
-            return View();
+            var CarPooling = await carpoolingDb.OppernigCarpooling.ToListAsync();
+            return View(CarPooling);
+            
         }
 
 
@@ -30,29 +30,29 @@ namespace CarPooling.Controllers
         }
 
         // Async Method
-        //[HttpPost]
-        //public async Task<IActionResult> OfferingCarpooling(Carpooling addcarpooling)
-        //{
-        //    var carpooling = new Carpooling()
-        //    {
-        //        PsNo = addcarpooling.PsNo,
-        //        EmployeeName = addcarpooling.EmployeeName,
-        //        EmailId = addcarpooling.EmailId,
-        //        Dept = addcarpooling.Dept,
-        //        Address = addcarpooling.Address,
-        //        Mobile = addcarpooling.Mobile,
-        //        Wheeler = addcarpooling.Wheeler,
-        //        BPoint = addcarpooling.BPoint,
-        //        Route = addcarpooling.Route,
-        //        LTime = addcarpooling.LTime,
-        //        ATime = addcarpooling.ATime,
-        //        Person = addcarpooling.Person,
-        //        Cost = addcarpooling.Cost
-        //    };
+        [HttpPost]
+        public async Task<IActionResult> OfferingCarpooling(Carpooling addcarpooling)
+        {
+            var carpooling = new Carpooling()
+            {
+                PsNo = addcarpooling.PsNo,
+                EmployeeName = addcarpooling.EmployeeName,
+                EmailId = addcarpooling.EmailId,
+                Dept = addcarpooling.Dept,
+                Address = addcarpooling.Address,
+                Mobile = addcarpooling.Mobile,
+                Wheeler = addcarpooling.Wheeler,
+                BPoint = addcarpooling.BPoint,
+                Route = addcarpooling.Route,
+                LTime = addcarpooling.LTime,
+                ATime = addcarpooling.ATime,
+                Person = addcarpooling.Person,
+                Cost = addcarpooling.Cost
+            };
 
-        //    await carpoolingDb.OppernigCarpooling.AddAsync(carpooling);
-        //    await carpoolingDb.SaveChangesAsync();
-        //    return RedirectToAction("Index");
-        //}
+            await carpoolingDb.OppernigCarpooling.AddAsync(carpooling);
+            await carpoolingDb.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
