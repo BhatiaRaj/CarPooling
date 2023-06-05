aaffcfe04fb0032c031705663445e3d29848e3d8
Raj Bhatia <97431540+BhatiaRaj@users.noreply.github.com>
2023-06-05 21:58:31 +0530

    Update ApplyRequectController.cs

--- CarPooling/Controllers/ApplyRequectController.cs
+++ CarPooling/Controllers/ApplyRequectController.cs
@@ -6,12 +6,12 @@ namespace CarPooling.Controllers
 {
     public class ApplyRequectController : Controller
     {
-        //private readonly CarpoolingDbContext applyRequectDb;
+        private readonly CarpoolingDbContext applyRequectDb;
 
-        //public ApplyRequectController(CarpoolingDbContext applyRequectDb)
-        //{
-        //    this.applyRequectDb = applyRequectDb;
-        //}
+        public ApplyRequectController(CarpoolingDbContext applyRequectDb)
+        {
+            this.applyRequectDb = applyRequectDb;
+        }
 
         [HttpGet]
         public IActionResult ApplyRequect()
@@ -20,22 +20,22 @@ namespace CarPooling.Controllers
         }
 
         //// Async Method
-        //[HttpPost]
-        //public async Task<IActionResult> ApplyRequect(ApplyRequect addApplyRequect)
-        //{
-        //    var ApplyRequect = new ApplyRequect()
-        //    {
-        //        UserName = addApplyRequect.UserName,
-        //        PsNo = addApplyRequect.PsNo,
-        //        PickUpPoint = addApplyRequect.PickUpPoint,
-        //        Mobile = addApplyRequect.Mobile,
-        //        Time = addApplyRequect.Time
+        [HttpPost]
+        public async Task<IActionResult> ApplyRequect(ApplyRequect addApplyRequect)
+        {
+            var ApplyRequect = new ApplyRequect()
+            {
+                UserName = addApplyRequect.UserName,
+                PsNo = addApplyRequect.PsNo,
+                PickUpPoint = addApplyRequect.PickUpPoint,
+                Mobile = addApplyRequect.Mobile,
+                Time = addApplyRequect.Time
 
-        //    };
+            };
 
-        //    await applyRequectDb.ApplyRequests.AddAsync(ApplyRequect);
-        //    await applyRequectDb.SaveChangesAsync();
-        //    return RedirectToAction("ApplyRequect");
-        //}
+            await applyRequectDb.ApplyRequests.AddAsync(ApplyRequect);
+            await applyRequectDb.SaveChangesAsync();
+            return RedirectToAction("ApplyRequect");
+        }
     }
 }
