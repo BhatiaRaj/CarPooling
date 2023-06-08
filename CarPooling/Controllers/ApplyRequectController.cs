cdb2217cf9f7e5a665e662700a4c6fbdbaa55fe2
Raj Bhatia <97431540+BhatiaRaj@users.noreply.github.com>
2023-06-08 22:45:13 +0530

    Commit

--- CarPooling/Controllers/ApplyRequectController.cs
+++ CarPooling/Controllers/ApplyRequectController.cs
@@ -19,6 +19,8 @@ namespace CarPooling.Controllers
             return View();
         }
 
+
+
         //// Async Method
         [HttpPost]
         public async Task<IActionResult> ApplyRequect(ApplyRequect addApplyRequect)
